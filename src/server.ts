a33593cddd12c202e118cf6845f97e4d79f08ae4
guykomash <gkomash@gmail.com>
2024-08-09 20:42:07 +0300

    PORT

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import app from './app';
 import mongoose from 'mongoose';
 import { createServer } from 'http';
 import { socket } from './socket';
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 const server = createServer(app);
 // Attach socket logic to server
@@ -10,5 +10,7 @@ socket(server);
 
 mongoose.connection.once('open', () => {
   console.log('Connected to MongoDB');
-  server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+  server.listen(PORT, '0.0.0.0', () =>
+    console.log(`Server is running on port ${PORT}`)
+  );
 });
